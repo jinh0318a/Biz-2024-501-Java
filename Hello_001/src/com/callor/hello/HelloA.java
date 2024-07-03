f1cf76a314567bf9bfaa65c49ba938fb7081a93c
jinh0318a <jinh0318a@naver.com>
2024-07-03 14:23:07 +0900

    Hello 실습

---
+++ Hello_001/src/com/callor/hello/HelloA.java
@@ -0,0 +1,12 @@
+package com.callor.hello;
+
+public class HelloA {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Korea");
+		System.out.println(30 + 40);
+		
+	}
+
+}
