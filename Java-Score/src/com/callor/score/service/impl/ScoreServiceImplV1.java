b1d40d617c40a7efde131c2991c4a1bffbdbe33a
jinh0318a <jinh0318a@naver.com>
2024-07-12 10:59:53 +0900

    fix V1

--- Java-Score/src/com/callor/score/service/impl/ScoreServiceImplV1.java
+++ Java-Score/src/com/callor/score/service/impl/ScoreServiceImplV1.java
@@ -9,6 +9,8 @@ import java.util.Scanner;
 
 import com.callor.score.models.ScoreDto;
 import com.callor.score.service.ScoreService;
+import com.callor.score.utils.Contract;
+import com.callor.score.utils.Line;
 
 /*
  * 생성자를 통해서 score.txt 파일의 경로 주입받기
@@ -19,42 +21,56 @@ import com.callor.score.service.ScoreService;
  */
 public class ScoreServiceImplV1 implements ScoreService {
 
-	protected final String file;
-	protected Scanner scan;
-	protected List<ScoreDto> scoreList = null;
+	protected final String scoreDataFile;
+	protected final Scanner fileReader;
+	protected final List<ScoreDto> scoreList;
 
-	public ScoreServiceImplV1(String file) throws FileNotFoundException {
-		this.file = file;
-		InputStream inputFile = null;
-		inputFile = new FileInputStream(this.file);
-		this.scan = new Scanner(inputFile);
-		this.scoreList = new ArrayList<ScoreDto>();
-
+	public ScoreServiceImplV1(String scoreDataFile) throws FileNotFoundException {
+		this.scoreDataFile = scoreDataFile;
+		InputStream inputFile = new FileInputStream(this.scoreDataFile);
+		this.fileReader = new Scanner(inputFile);
+		this.scoreList = new ArrayList<>();
 	}
 
 	@Override
 	public void loadScoreData() {
-		scan.nextLine();
-		while (scan.hasNext()) {
-			String[] arr = scan.nextLine().split(",");
+		fileReader.nextLine();
+		while (fileReader.hasNext()) {
+			String[] arr = fileReader.nextLine().split(",");
 			ScoreDto one = new ScoreDto();
-			one.sc_num = arr[0];
-			one.sc_kor = Integer.valueOf(arr[1]);
-			one.sc_eng = Integer.valueOf(arr[2]);
-			one.sc_math = Integer.valueOf(arr[3]);
-			one.sc_music = Integer.valueOf(arr[4]);
-			one.sc_art = Integer.valueOf(arr[5]);
-			one.sc_sw = Integer.valueOf(arr[6]);
-			one.sc_db = Integer.valueOf(arr[7]);
+			one.sc_num = arr[Contract.Score.학번];
+			one.sc_kor = Integer.valueOf(arr[Contract.Score.국어]);
+			one.sc_eng = Integer.valueOf(arr[Contract.Score.영어]);
+			one.sc_math = Integer.valueOf(arr[Contract.Score.수학]);
+			one.sc_music = Integer.valueOf(arr[Contract.Score.음악]);
+			one.sc_art = Integer.valueOf(arr[Contract.Score.미술]);
+			one.sc_sw = Integer.valueOf(arr[Contract.Score.소프트웨어공학]);
+			one.sc_db = Integer.valueOf(arr[Contract.Score.데이터베이스]);
 
 			this.scoreList.add(one);
 		}
 	}
-
+	
+	/*
+	 * ==============================
+	 * 성적표
+	 * ---------------------
+	 * 학번 국어 영어 수학 ... 총점 평균 
+	 * ----------------------
+	 * 
+	 * ===============================
+	 * 
+	 */
 	@Override
 	public void printScoreList() {
 		for (ScoreDto one : this.scoreList) {
-			System.out.println(one.toString());
+			System.out.println(Line.dLine(100));
+			System.out.println(" 성적표");
+			System.out.println(Line.sLine(100));
+			System.out.println(" 학번 국어 영어 수학 음악 미술 SW DB 총점 평균");
+			System.out.println(Line.sLine(100));
+			System.out.printf("%s  %d   %d   %d   %d   %d  %d %d %d %.2f \n",one.sc_num, one.sc_kor, one.sc_eng, one.sc_math, one.sc_music, one.sc_art, one.sc_sw, one.sc_db, one.total(), one.average());
+			System.out.println(Line.dLine(100));
 		}
 	}
 
---
+++ Java-Score/src/com/callor/score/utils/Contract.java
@@ -0,0 +1,14 @@
+package com.callor.score.utils;
+
+public class Contract {
+	public static class Score{
+		public final static int 학번 = 0; 
+		public final static int 국어 = 1;
+		public final static int 영어 = 2;
+		public final static int 수학 = 3;
+		public final static int 음악 = 4;
+		public final static int 미술 = 5;
+		public final static int 소프트웨어공학 = 6;
+		public final static int 데이터베이스 = 7;
+	}
+}
